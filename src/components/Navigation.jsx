5246765bd019fdf9cca0ef96a8d19a5fc3c30c4a
james.mendoza <james.mendoza@rappi.com>
2023-07-11 14:53:20 -0500

    Add links and mobile responsive

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import { Button, Container, Box, Typography } from "@mui/material";
-import { FacebookRounded, Instagram, WhatsApp } from "@mui/icons-material";
+import {
+  Button,
+  Container,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import {
+  FacebookRounded,
+  Instagram,
+  WhatsApp,
+  Menu as MenuIcon,
+} from "@mui/icons-material";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -14,46 +27,69 @@ const sections = [
   {
     name: "about",
     label: "Nosotros",
-    link: "/",
+    link: "/#about-us",
+  },
+  {
+    name: "services",
+    label: "Servicios",
+    link: "/#services",
   },
   {
     name: "products",
     label: "Productos",
-    link: "/",
+    link: "/#products",
   },
   {
     name: "contact",
     label: "Contacto",
-    link: "/",
+    link: "/#contact",
   },
 ];
 const socialMedia = [
   {
     name: "Facebook",
     icon: FacebookRounded,
-    link: "/",
+    link: "https://www.facebook.com/devend.seguridad",
+    target: "_blank",
   },
   {
     name: "Instagram",
     icon: Instagram,
-    link: "/",
+    link: "https://www.instagram.com/devend.seguridad/",
+    target: "_blank",
   },
   {
     name: "TikTok",
     iconSource: "/icons/tiktok.svg",
-    link: "/",
+    link: "https://www.tiktok.com/@devend.seguridad",
+    target: "_blank",
   },
   {
     name: "WhatsApp",
     icon: WhatsApp,
     link: "/",
+    target: "_blank",
   },
 ];
 
 export default function Navigation() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <AppBar position="static">
-      <Container maxWidth={false} disableGutters sx={{ maxWidth: "1280px" }}>
+    <AppBar position="fixed" component="header">
+      <Container
+        maxWidth={false}
+        disableGutters
+        sx={{ maxWidth: "1280px", padding: "0 20px" }}
+      >
         <Box
           display="grid"
           justifyContent="space-between"
@@ -66,16 +102,55 @@ export default function Navigation() {
             justifyContent="space-between"
             gridTemplateColumns="repeat(2, max-content)"
             alignItems="center"
-            gap="30px"
+            sx={{ gap: { sx: "0", md: "30px" } }}
           >
-            <Image
-              src="/logoDevend.svg"
-              alt="Picture of the author"
-              width={135}
-              height={39}
-            />
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {sections.map(({ name, link, label }) => (
+                  <MenuItem key={name} href={link} LinkComponent={Link}>
+                    {label}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
 
-            <Box display="flex" gap="15px">
+            <Link href="/">
+              <Image
+                src="/logoDevend.svg"
+                alt="Picture of the author"
+                width={135}
+                height={39}
+              />
+            </Link>
+
+            <Box sx={{ display: { xs: "none", md: "flex" } }} gap="15px">
               {sections.map(({ name, link, label }) => (
                 <Link key={name} href={link} sx={{}}>
                   <Button
@@ -95,29 +170,32 @@ export default function Navigation() {
             gridAutoFlow="column"
             gap="10px"
           >
-            {socialMedia.map(({ icon: Icon, iconSource, name, link }) => {
-              return (
-                <Link
-                  key={name}
-                  href={link}
-                  style={{
-                    color: "#fff",
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  {Icon ? (
-                    <Icon />
-                  ) : (
-                    <img
-                      src={iconSource}
-                      alt={name}
-                      style={{ width: "20px", height: "20px" }}
-                    />
-                  )}
-                </Link>
-              );
-            })}
+            {socialMedia.map(
+              ({ icon: Icon, iconSource, name, link, ...props }) => {
+                return (
+                  <Link
+                    key={name}
+                    href={link}
+                    style={{
+                      color: "#fff",
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                    {...props}
+                  >
+                    {Icon ? (
+                      <Icon />
+                    ) : (
+                      <img
+                        src={iconSource}
+                        alt={name}
+                        style={{ width: "20px", height: "20px" }}
+                      />
+                    )}
+                  </Link>
+                );
+              }
+            )}
           </Box>
         </Box>
       </Container>
