deba6ffb4a2f5a10079fc5dbf6fae278a86269ad
james.mendoza <james.mendoza@rappi.com>
2023-07-11 14:52:13 -0500

    Add Section component

---
+++ src/components/Section.jsx
@@ -0,0 +1,34 @@
+import { Box } from "@mui/material";
+
+export default function Section({
+  id,
+  name,
+  children,
+  fullHeight = false,
+  background = "transparent",
+}) {
+  return (
+    <Box
+      id={id}
+      name={name}
+      component="section"
+      sx={{
+        display: "grid",
+        alignItems: "center",
+        minHeight: fullHeight ? "calc(100vh)" : "400px",
+        padding: "50px 0",
+        background,
+      }}
+    >
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "1fr",
+          padding: "0 20px",
+        }}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+}
