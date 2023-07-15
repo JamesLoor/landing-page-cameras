208e9b8f37fa463e5e30b041931d8a98ed2062be
james.mendoza <james.mendoza@rappi.com>
2023-07-14 22:35:08 -0500

    Add constants by sections and socialMedia objects

---
+++ src/constants/sections.jsx
@@ -0,0 +1,27 @@
+export const sections = [
+  {
+    name: "home",
+    label: "Inicio",
+    link: "/",
+  },
+  {
+    name: "about",
+    label: "Nosotros",
+    link: "/#about-us",
+  },
+  {
+    name: "services",
+    label: "Servicios",
+    link: "/#services",
+  },
+  {
+    name: "package",
+    label: "Paquetes",
+    link: "/#package",
+  },
+  {
+    name: "contact",
+    label: "Contacto",
+    link: "/#contact",
+  },
+];
---
+++ src/constants/socialMedia.jsx
@@ -0,0 +1,28 @@
+import { FacebookRounded, Instagram, WhatsApp } from "@mui/icons-material";
+
+export const socialMedia = [
+  {
+    name: "Facebook",
+    icon: FacebookRounded,
+    link: "https://www.facebook.com/devend.seguridad",
+    target: "_blank",
+  },
+  {
+    name: "Instagram",
+    icon: Instagram,
+    link: "https://www.instagram.com/devend.seguridad/",
+    target: "_blank",
+  },
+  {
+    name: "TikTok",
+    iconSource: "/icons/tiktok.svg",
+    link: "https://www.tiktok.com/@devend.seguridad",
+    target: "_blank",
+  },
+  {
+    name: "WhatsApp",
+    icon: WhatsApp,
+    link: "https://wa.me/+593963310383",
+    target: "_blank",
+  },
+];
