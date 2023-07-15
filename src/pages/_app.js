0dfe3f5dc372c43f978463e78370c61bda841713
james.mendoza <james.mendoza@rappi.com>
2023-07-14 23:53:47 -0500

    Revert "Add facebook pixel"
    This reverts commit 1066f537752f14d64fbb63722ff4189613b8beb2.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,27 +6,11 @@ import { CacheProvider } from "@emotion/react";
 import theme from "@/utils/theme";
 import createEmotionCache from "@/utils/createEmotionCache";
 import "@/styles/globals.css";
-import { useRouter } from "next/router";
-import * as fbq from "@/utils/fpixel";
 
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
-  const router = useRouter();
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
-  React.useEffect(() => {
-    fbq.pageview();
-
-    const handleRouteChange = () => {
-      fbq.pageview();
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <CacheProvider value={emotionCache}>
