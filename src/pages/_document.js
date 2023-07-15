887095afb6153bfd749b9ece0d6003961416cfe2
james.mendoza <james.mendoza@rappi.com>
2023-07-15 00:11:55 -0500

    Add facebook pixel v3

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
 import theme from "@/utils/theme";
 import createEmotionCache from "@/utils/createEmotionCache";
-import Script from "next/script";
+import { FB_PIXEL_ID } from "../utils/fpixel";
 
 export default function MyDocument(props) {
   const { emotionStyleTags } = props;
@@ -12,25 +12,14 @@ export default function MyDocument(props) {
   return (
     <Html lang="en" style={{ scrollBehavior: "smooth" }}>
       <Head>
-        {/* PWA primary color */}
-        <Script
-          id="fb-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1310609789848862');
-              fbq('track', 'PageView');
-              `,
-          }}
-        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
         <meta name="theme-color" content={theme.palette.primary.main} />
         <link rel="shortcut icon" href="/favicon.svg" />
         <meta name="emotion-insertion-point" content="" />
