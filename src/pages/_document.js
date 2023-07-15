cfc04d93276e8737ad6aba93d23a75715076d455
james.mendoza <james.mendoza@rappi.com>
2023-07-15 11:03:44 -0500

    Add GTM script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,7 @@ import createEmotionServer from "@emotion/server/create-instance";
 import theme from "@/utils/theme";
 import createEmotionCache from "@/utils/createEmotionCache";
 import { FB_PIXEL_ID } from "@/utils/fpixel";
+import Script from "next/script";
 
 export default function MyDocument(props) {
   const { emotionStyleTags } = props;
@@ -14,35 +15,29 @@ export default function MyDocument(props) {
   return (
     <Html lang="en" style={{ scrollBehavior: "smooth" }}>
       <Head>
-        <script
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', ${FB_PIXEL_ID});
-            `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TDMDTQC');`,
           }}
-        ></script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-          />
-        </noscript>
+        />
         <meta name="theme-color" content={theme.palette.primary.main} />
         <link rel="shortcut icon" href="/favicon.svg" />
         <meta name="emotion-insertion-point" content="" />
         {emotionStyleTags}
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDMDTQC"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
